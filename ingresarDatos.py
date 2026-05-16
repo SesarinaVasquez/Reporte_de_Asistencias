07b48f908f71e4f269a0ba6c1b2a44f547948ee5
SesarinaVasquez <106773753+SesarinaVasquez@users.noreply.github.com>
2026-05-16 00:36:51 -0500

    Add functions for user registration and data input

---
+++ ingresarDatos.py
@@ -0,0 +1,18 @@
+def registro():
+    codigo = "00001"
+    nombre = "Juan"
+    area = "Contabilidad"
+    turno = "Tarde"
+    aprobador = "Jefe"
+    
+registro()
+
+
+def datos():
+    codigo = input("Ingrese código de usuario: ")
+    nombre = input("Ingrese nombres y apellidos: ")
+    area = input("Ingrese área: ")
+    turno = input("Ingrese turno: ")
+    aprobador = input("Ingrese aprobador: ")
+    
+datos()
